f02f885183c7a6ae0c76ea78d5516a0cb7ab5608
Shikhar Gupta <64977179+shikhargupta02@users.noreply.github.com>
2022-01-03 00:22:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -48,26 +48,4 @@ $(document).ready(function() {
         loop: true
     });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                menu: false
-            },
-            600: {
-                items: 2,
-                menu: false
-            },
-            1000: {
-                items: 3,
-                menu: false
-            }
-        }
-    });
 });
